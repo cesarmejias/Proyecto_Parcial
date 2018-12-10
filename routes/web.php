4e35a0886375851f77d4ee4338be0afb296780d9
C <cesarmejiasupta@gmail.com>
2018-12-10 16:18:41 -0400

    Rutas agregadas

--- routes/web.php
+++ routes/web.php
@@ -11,18 +11,42 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
 
 Auth::routes();
 
-Route::get('/', 'TransactionController@index')->name('home');
+   Route::group(['middleware' => 'auth'], function (){
 
-Route::resource('users', 'UserController');
+   Route::get('/', 'UserController@index')->name('users.index');
 
-Route::resource('roles', 'RoleController');
+   Route::resource('users', 'UserController');
 
-Route::resource('permissions', 'PermissionController');
+   Route::resource('roles', 'RoleController');
 
-Route::resource('posts', 'PostController');
+   Route::resource('permissions', 'PermissionController');
+
+   Route::resource('transactions', 'TransactionController');
+
+
+
+   
+     Route::get('transactions', 'TransactionController@index')->name('transactions.index')
+                                                         ->middleware('permission:transactions.index');
+
+     Route::post('transactions/store', 'TransactionController@store')->name('transactions.store')
+                                                         ->middleware('permission:transactions.create');
+
+     Route::get('transactions/create', 'TransactionController@create')->name('transactions.create')
+                                                         ->middleware('permission:transactions.create');
+    
+     Route::put('transactions/{role}', 'TransactionController@update')->name('transactions.update')
+                                                        ->middleware('permission:transactions.edit');
+                                                                           // ->middleware('role:Admin');
+
+     Route::get('transactions/{role}', 'TransactionController@show')->name('transactions.show')
+                                                         ->middleware('permission:transactions.show');
+     Route::delete('transactions/{role}', 'TransactionController@destroy')->name('transactions.destroy')
+                                                         ->middleware('permission:transactions.destroy');
+     Route::get('transactions/{role}/edit', 'TransactionController@edit')->name('transactions.edit')
+                                                         ->middleware('permission:transactions.edit');
+
+});
